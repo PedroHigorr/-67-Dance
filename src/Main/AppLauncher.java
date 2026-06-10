2f40d47d6f9fc0932e37d8f7ee834cb7e3f8cddc
Pedro Higor <pedro.higor92@gmail.com>
2026-06-10 18:51:52 -0300

    Ajustando Main para inicializar gráficos

---
+++ src/Main/AppLauncher.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        // O Cavalo de Troia chama a sua classe Main verdadeira por debaixo dos panos
+        Main.main(args);
+    }
+}
